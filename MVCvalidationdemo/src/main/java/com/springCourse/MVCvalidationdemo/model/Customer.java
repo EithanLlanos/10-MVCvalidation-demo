81af74d760cf587d709b5d807d9137bc477bfd91
Rubick <eithanllanosb@gmail.com>
2024-12-16 18:12:04 -0500

    Add field with pattern annotation and remake messages

--- MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/model/Customer.java
+++ MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/model/Customer.java
@@ -1,9 +1,6 @@
 package com.springCourse.MVCvalidationdemo.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -14,9 +11,12 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
-    @Min(value = 0, message = "Must be greater or equal than 0")
-    @Max(value = 10, message = "Must be lower or equal than 10")
+    @Min(value = 0, message = "must be greater or equal than 0")
+    @Max(value = 10, message = "must be lower or equal than 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public Customer() {
 
@@ -38,13 +38,21 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    @Min(value = 0, message = "Must be greater or equal than 0")
-    @Max(value = 10, message = "Must be lower or equal than 10")
+    @Min(value = 0, message = "must be greater or equal than 0")
+    @Max(value = 10, message = "must be lower or equal than 10")
     public int getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(@Min(value = 0, message = "Must be greater or equal than 0") @Max(value = 10, message = "Must be lower or equal than 10") int freePasses) {
+    public void setFreePasses(@Min(value = 0, message = "must be greater or equal than 0") @Max(value = 10, message = "must be lower or equal than 10") int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits") String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(@Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits") String postalCode) {
+        this.postalCode = postalCode;
     }
 }
