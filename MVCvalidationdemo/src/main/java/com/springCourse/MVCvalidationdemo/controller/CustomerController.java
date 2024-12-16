b1bd129fe369500c63e14f904866ba16503550ab
Rubick <eithanllanosb@gmail.com>
2024-12-16 17:06:54 -0500

    Set up simple MVC implementation

--- MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/controller/CustomerController.java
+++ MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.springCourse.MVCvalidationdemo.controller;
 
 import com.springCourse.MVCvalidationdemo.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +20,8 @@ public class CustomerController {
     }
 
     @PostMapping("/customerView")
-    public String getCustomerView(@ModelAttribute("customer") Customer theCustomer) {
-        return "customer-view";
+    public String getCustomerView(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+        if (theBindingResult.hasFieldErrors()) return "customer-form";
+        else return "customer-view";
     }
 }
--- MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/model/Customer.java
+++ MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/model/Customer.java
@@ -1,8 +1,16 @@
 package com.springCourse.MVCvalidationdemo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Customer {
 
     private String firstName;
+
+    @NotBlank(message = "Last name is required.") // Ensures the field is not null, empty, or only whitespace.
+    @Size(max = 50, message = "Last name must not exceed 50 characters.") // Limits the length of the last name.
+    @Pattern(regexp = "^[A-Za-z '-]+$", message = "Last name can only contain letters, spaces, hyphens, and apostrophes.")
     private String lastName;
 
     public Customer() {
