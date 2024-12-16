72c1d79640cc18bbb051b10be4a23aaa3e219b5b
Rubick <eithanllanosb@gmail.com>
2024-12-16 17:25:31 -0500

    Add freePasses field and refactor getter and setter methods from Customer Java

--- MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/model/Customer.java
+++ MVCvalidationdemo/src/main/java/com/springCourse/MVCvalidationdemo/model/Customer.java
@@ -1,5 +1,7 @@
 package com.springCourse.MVCvalidationdemo.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,6 +13,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "Must be greater or equal than 0")
+    @Max(value = 10, message = "Must be lower or equal than 10")
+    private int freePasses;
 
     public Customer() {
 
@@ -24,11 +30,21 @@ public class Customer {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
+    public @NotNull(message = "is required") @Size(min = 1, message = "is required") String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(@NotNull(message = "is required") @Size(min = 1, message = "is required") String lastName) {
         this.lastName = lastName;
+    }
+
+    @Min(value = 0, message = "Must be greater or equal than 0")
+    @Max(value = 10, message = "Must be lower or equal than 10")
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(@Min(value = 0, message = "Must be greater or equal than 0") @Max(value = 10, message = "Must be lower or equal than 10") int freePasses) {
+        this.freePasses = freePasses;
     }
 }
